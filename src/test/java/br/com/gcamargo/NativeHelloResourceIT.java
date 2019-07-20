49cac7ef11fb53efe348c7c7d73a20e37dad1312
GuilhermeOCamargo <guilherme.ocamargo@gmail.com>
2019-07-20 16:03:15 -0300

    Project Creation

---
+++ src/test/java/br/com/gcamargo/NativeHelloResourceIT.java
@@ -0,0 +1,9 @@
+package br.com.gcamargo;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeHelloResourceIT extends HelloResourceTest {
+
+    // Execute the same tests but in native mode.
+}
